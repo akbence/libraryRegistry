cbe222dbc91d45577c702a0bf0da04184aceb7c7
Ágocsi-Kiss Bence <bence.agocsi-kiss@icellmobilsoft.hu>
2020-02-12 16:45:57 +0100

    creating models: book,category

--- libraryManager/models.py
+++ libraryManager/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Book(models.Model):
+    author = models.CharField(max_length=50)
+    title = models.CharField(max_length=50)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    page = models.PositiveIntegerField()
+    already_read = models.BooleanField(default=False)
+    date_of_read = models.DateField()
