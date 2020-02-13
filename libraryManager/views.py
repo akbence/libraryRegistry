ff6111d32d81e2f8afc2b12d6f7da904700bf224
Ágocsi-Kiss Bence <akbence007@gmail.com>
2020-02-13 01:10:31 +0100

    django shortcut added

--- libraryManager/views.py
+++ libraryManager/views.py
@@ -1,17 +1,15 @@
 from django.shortcuts import render
 from django.http import response
-from django.template import loader
 
 from libraryManager.models import Book
 
 
 def index(request):
     all_books = Book.objects.all()
-    template = loader.get_template('libraryManager/index.html')
     context = {
         'all_books': all_books
     }
-    return response.HttpResponse(template.render(context,request))
+    return render(request, 'libraryManager/index.html', context)
 
 
 def detail(request, book_id):
