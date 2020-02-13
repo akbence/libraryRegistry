fb7fe0dc1988dfbf98890ae16ad58f874da1a57f
Ágocsi-Kiss Bence <akbence007@gmail.com>
2020-02-13 14:40:19 +0100

    html logic with read_date, hardcoded url remove

--- libraryManager/urls.py
+++ libraryManager/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('book/<int:book_id>', views.detail)
+    path('', views.index,name='index'),
+    path('book/<int:book_id>', views.detail,name='detail')
 ]
