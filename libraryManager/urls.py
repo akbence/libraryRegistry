63a96280a950487b6a4281e8831c8baa2d8ee69d
Ágocsi-Kiss Bence <akbence007@gmail.com>
2020-02-17 01:20:06 +0100

    requirements.txt added, most endpoints guarded by login

--- libraryManager/urls.py
+++ libraryManager/urls.py
@@ -6,12 +6,12 @@ app_name = 'libraryManager'
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('query', views.QueryView.as_view(), name='query'),
+    path('query', login_required(views.QueryView.as_view()), name='query'),
     path('login', views.LoginFormView.as_view(), name='login'),
     path('logout', views.logout_view, name='logout'),
     path('register', views.RegisterFormView.as_view(), name='register'),
     path('book/<pk>', login_required(views.DetailView.as_view()), name='detail'),
-    path('add', views.BookCreate.as_view(), name='add-form'),
-    path('update/<pk>', views.BookUpdate.as_view(), name='update-form'),
-    path('delete/<pk>', views.BookDelete.as_view(), name='delete'),
+    path('add', login_required(views.BookCreate.as_view()), name='add-form'),
+    path('update/<pk>', login_required(views.BookUpdate.as_view()), name='update-form'),
+    path('delete/<pk>', login_required(views.BookDelete.as_view()), name='delete'),
 ]
